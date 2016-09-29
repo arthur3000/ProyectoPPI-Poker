637cfac7f59e4efec9f8818ba5cc923c613f024b
AndoniGuzman <andoni042295@gmail.com>
2016-09-28 23:18:09 -0500

    Ventana Juego
    Se soluciono el bug de las cartas y se agrego una etiqueta para el dinero que se manejara.

--- ventanaJuego.cpp
+++ ventanaJuego.cpp
@@ -1,6 +1,10 @@
+
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
-
+//#include <Text.hpp> // Para las etiquetas de texto dentro del juego.
+#include <iostream>
+#include "mazo.h"
+#include "carta.h"
 #define ALTURA_PANTALLA 720
 #define ANCHURA_PANTALLA 1280
 // Posicion de las cartas en la interfaz
@@ -20,10 +24,20 @@
 #define CARTA_CINCO_MESA_Y 80
 #define FICHAS_X 80
 #define FICHAS_Y 350
-
-
+#define TAMANO_TIPOGRAFIA 75
+#define POSICION_X_DINERO 120
+#define POSICION_Y_DINERO 530
+using namespace std;
 int main(){
-
+    // Strings
+    string nombreCartaUnoJugador;
+    string nombreCartaDosJugador;
+    bool cartasJugadorRepartidas  = false;; // Para saber si las cartas ya fueron repartidas;
+    // Carta
+    Carta temp;
+    //  Mazo
+    Mazo mazo;
+    mazo.inicializarMazo(); // Da el nombre que necesitan las cartas para su posterior utilizacion
     sf::RenderWindow window(sf::VideoMode(ANCHURA_PANTALLA, ALTURA_PANTALLA), "Texas Hold'em"); // La resolucion puede cambiar, pero se necesitaria cambiar tambien las imagenes.
     // Texturas
     sf::Texture fondo; // Se guarda la imagen de fondo
@@ -47,6 +61,17 @@ int main(){
     sf::Sprite sCartaCincoMesa;
     sf::Sprite spriteFichas;
 
+    sf::Font font;
+    if (!font.loadFromFile("comicz.ttf")){
+        cout << "Error " << endl;
+    }
+    sf::Text dineroJugador; // Dinero que el jugador tiene
+    dineroJugador.setFont(font);
+    dineroJugador.setCharacterSize(TAMANO_TIPOGRAFIA);
+    dineroJugador.setColor(sf::Color::Blue);
+    dineroJugador.setStyle(sf::Text::Bold | sf::Text::Underlined); // Parametros que llevara el texto
+    dineroJugador.setPosition(POSICION_X_DINERO,POSICION_Y_DINERO);
+
     while(window.isOpen()){
     	sf::Event event;
     	while(window.pollEvent(event)){ // Verifica los eventos de la ventana
@@ -60,20 +85,30 @@ int main(){
     	else{
     		// Inicializacion de cartas
     		// Cartas Mano
-    		cartaUnoJugador.loadFromFile("Joker.png");
-    		cartaDosJugador.loadFromFile("Joker.png");
+
+    		if(!cartasJugadorRepartidas){ // Para que solo se tengan que repartir una vez;
+    			temp = mazo.repartirCartaJugador();
+    			cartaUnoJugador.loadFromFile(temp.nombre);
+    			temp = mazo.repartirCartaJugador();
+    			cartaDosJugador.loadFromFile(temp.nombre);
+    			cartasJugadorRepartidas = true;
+
+    		}
+
+
             // Cartas Mesa
-            cartaUnoMesa.loadFromFile("Joker.png");
-            cartaDosMesa.loadFromFile("Joker.png");
-            cartaTresMesa.loadFromFile("Joker.png");
-            cartaCuatroMesa.loadFromFile("Joker.png");
-            cartaCincoMesa.loadFromFile("Joker.png");
+
+            cartaUnoMesa.loadFromFile("Reverso.png");
+            cartaDosMesa.loadFromFile("Reverso.png");
+            cartaTresMesa.loadFromFile("Reverso.png");
+            cartaCuatroMesa.loadFromFile("Reverso.png");
+            cartaCincoMesa.loadFromFile("Reverso.png");
 
             // Fichas
             modeloFichas.loadFromFile("modeloFichas.png");
 
 			// Texturas carta Mano
-    		sCartaUnoJugador.setTexture(cartaUnoJugador);
+            sCartaUnoJugador.setTexture(cartaUnoJugador);
     		sCartaDosJugador.setTexture(cartaDosJugador);
 
     		// Texturas Carta Mesa
@@ -102,9 +137,14 @@ int main(){
 
     		spriteFichas.setPosition(FICHAS_X,FICHAS_Y);
 
+    		// Dinero del jugador
+            dineroJugador.setString("1000"); // Cambiar por el valorr que tiene la clase jugador.dinero
+
+
     		background.setTexture(fondo); // Se pone la textura de fondo
     		window.draw(background); 	// Se agrega a la ventana
     		// Se agregan las cartas de jugador
+
     		window.draw(sCartaUnoJugador);
     		window.draw(sCartaDosJugador);
     		// Se agregan las cartas de la mesa
@@ -114,8 +154,10 @@ int main(){
     		window.draw(sCartaCuatroMesa);
     		window.draw(sCartaCincoMesa);
     		window.draw(spriteFichas);
-
+    		window.draw(dineroJugador);
     	}
+
+
 
     	window.display(); // Se muestra la ventana
 
